46ca77e8fd2a7d455ebaecb9479ce780808fdc8e
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-07-17 19:08:18 +0200

    [DEV] SEOS-257: add needed defines in mbedtls config file

--- configs/MbedTLS_Config.h
+++ configs/MbedTLS_Config.h
@@ -4,5 +4,9 @@
 #include "mbedtls/check_config.h"
 
 #define MBEDTLS_BASE64_C
+#define MBEDTLS_SHA256_C
+#define MBEDTLS_SHA512_C
+#define MBEDTLS_ENTROPY_C
+#define MBEDTLS_NO_PLATFORM_ENTROPY
 
 #endif /* MBEDTLS_CONFIG_H */
