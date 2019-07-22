c9cd7a45b61865261c5b74a8cc658b73dbbdfcb5
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-07-22 10:56:50 +0200

    [DEV] SEOS-94: Adding SeosKeyStoreApi wrappers around KeyStore functions for local usage.

--- include/seos/SeosKeyStoreApi.h
+++ include/seos/SeosKeyStoreApi.h
@@ -47,4 +47,115 @@ SeosKeyStoreApi_initAsRpc(SeosKeyStoreApi* self, SeosKeyStoreClient* client);
 void
 SeosKeyStoreApi_deInit(SeosKeyStoreApi* self);
 
+/***************************** KeyStore functions *******************************/
+/**
+ * @brief Imports a SeosCrypto_Key object into the keystore
+ *
+ * @param self          pointer to self
+ * @param name          name of the key to import
+ * @param key           key object to import
+ *
+ * @return seos_err
+ *
+ */
+seos_err_t
+SeosKeyStoreApi_importKey(SeosKeyStoreApi*  self,
+                            const char*     name,
+                            SeosCryptoKey*  key);
+/**
+ * @brief Retreives the key with a given name from the keystore
+ *
+ * @param self          pointer to self
+ * @param name          name of the key to get
+ * @param keyBytes      pointer to the allocated chunk of memory for the keyBytes
+ * @param keyBytes      pointer to the key type
+ * @param key[out]      the returned key
+ *
+ * @return seos_err
+ *
+ */
+seos_err_t
+SeosKeyStoreApi_getKey(SeosKeyStoreApi*         self,
+                        const char*             name,
+                        SeosCryptoKey*          key,
+                        char*                   keyBytes,
+                        SeosKeyStore_KeyType*   keyType);
+/**
+ * @brief Reads the key data from the key specified by the passed name and
+ * stores the key size in the output parameter keySize
+ *
+ * @param self          pointer to self
+ * @param name          name of the key we want to get
+ * @param keySize[out]  the size of the key in bytes
+ *
+ * @return seos_err
+ *
+ */
+seos_err_t
+SeosKeyStoreApi_getKeySizeBytes(SeosKeyStoreApi*    self,
+                                const char*         name,
+                                size_t*             keySize);
+/**
+ * @brief Deletes a key with a given name from the keystore
+ *
+ * @param self          pointer to self
+ * @param name          name of the key we want to delete
+ *
+ * @return seos_err
+ *
+ */
+seos_err_t
+SeosKeyStoreApi_deleteKey(SeosKeyStoreApi*      self,
+                            const char*         name);
+/**
+ * @brief Copies the key with a selected name from the current key store to
+ * the destination key store
+ *
+ * @param self          pointer to self
+ * @param name          name of the key we want to delete
+ * @param destKeyStore  pointer to the destination key store
+ *
+ * @return seos_err
+ *
+ */
+seos_err_t
+SeosKeyStoreApi_copyKey(SeosKeyStoreApi*    self,
+                        const char*         name,
+                        SeosKeyStore*       destKeyStore);
+/**
+ * @brief Moves the key with a selected name from the current key store to
+ * the destination key store (after the operation the key is no longer in the
+ * current key store)
+ *
+ * @param self          pointer to self
+ * @param name          name of the key we want to delete
+ * @param destKeyStore  pointer to the destination key store
+ *
+ * @return seos_err
+ *
+ */
+seos_err_t
+SeosKeyStoreApi_moveKey(SeosKeyStoreApi*    self,
+                        const char*         name,
+                        SeosKeyStore*       destKeyStore);
+/**
+ * @brief Generates a key with a given name using an RNG, stores the key into the key store
+ * and returns the key data in the key object.
+ *
+ * @param self          pointer to self
+ * @param name          name of the key we want to delete
+ * @param keyBytes      pointer to the allocated chunk of memory for the keyBytes
+ * @param keyBytes      pointer to the key type
+ * @param key[out]      the returned key
+ *
+ * @return seos_err
+ *
+ */
+seos_err_t
+SeosKeyStoreApi_generateKey(SeosKeyStoreApi*        self,
+                            SeosCryptoKey*          key,
+                            const char*             name, 
+                            char*                   keyBytes, 
+                            SeosKeyStore_KeyType*   keyType);
+
 /** @} */
--- src/seos/SeosKeyStoreApi.c
+++ src/seos/SeosKeyStoreApi.c
@@ -9,7 +9,6 @@
 // Private static functions ----------------------------------------------------
 
 // Public functions ------------------------------------------------------------
-
 seos_err_t
 SeosKeyStoreApi_initAsLocal(SeosKeyStoreApi* self, SeosKeyStore* keyStore)
 {
@@ -49,5 +48,126 @@ SeosKeyStoreApi_initAsRpc(SeosKeyStoreApi* self, SeosKeyStoreClient* client)
 void
 SeosKeyStoreApi_deInit(SeosKeyStoreApi* self)
 {
+    Debug_ASSERT_SELF(self);
     return;
 }
+
+seos_err_t
+SeosKeyStoreApi_importKey(SeosKeyStoreApi* self, const char* name, SeosCryptoKey*  key)
+{
+    Debug_ASSERT_SELF(self);
+    seos_err_t retval = SEOS_ERROR_GENERIC;
+
+    if (self->isLocalConnection)
+    {
+        retval = SeosKeyStore_importKey(self->connector.local.keyStore, name, key);
+    }
+    else
+    {
+        // implement client call
+    }
+    return retval;
+}
+
+seos_err_t
+SeosKeyStoreApi_getKey(SeosKeyStoreApi* self, const char* name, SeosCryptoKey* key, char* keyBytes, SeosKeyStore_KeyType* keyType)
+{
+    Debug_ASSERT_SELF(self);
+    seos_err_t retval = SEOS_ERROR_GENERIC;
+
+    if (self->isLocalConnection)
+    {
+        retval = SeosKeyStore_getKey(self->connector.local.keyStore, name, key, keyBytes, keyType);
+    }
+    else
+    {
+        // implement client call
+    }
+    return retval;
+}
+
+seos_err_t
+SeosKeyStoreApi_getKeySizeBytes(SeosKeyStoreApi* self, const char* name, size_t* keySize)
+{
+    Debug_ASSERT_SELF(self);
+    seos_err_t retval = SEOS_ERROR_GENERIC;
+
+    if (self->isLocalConnection)
+    {
+        retval = SeosKeyStore_getKeySizeBytes(self->connector.local.keyStore, name, keySize);
+    }
+    else
+    {
+        // implement client call
+    }
+    return retval;
+}
+
+seos_err_t
+SeosKeyStoreApi_deleteKey(SeosKeyStoreApi* self, const char* name)
+{
+    Debug_ASSERT_SELF(self);
+    seos_err_t retval = SEOS_ERROR_GENERIC;
+
+    if (self->isLocalConnection)
+    {
+        retval = SeosKeyStore_deleteKey(self->connector.local.keyStore, name);
+    }
+    else
+    {
+        // implement client call
+    }
+    return retval;
+}
+
+seos_err_t
+SeosKeyStoreApi_copyKey(SeosKeyStoreApi* self, const char* name, SeosKeyStore* destKeyStore)
+{
+    Debug_ASSERT_SELF(self);
+    seos_err_t retval = SEOS_ERROR_GENERIC;
+
+    if (self->isLocalConnection)
+    {
+        retval = SeosKeyStore_copyKey(self->connector.local.keyStore, name, destKeyStore);
+    }
+    else
+    {
+        // implement client call
+    }
+    return retval;
+}
+
+seos_err_t
+SeosKeyStoreApi_moveKey(SeosKeyStoreApi* self, const char* name, SeosKeyStore* destKeyStore)
+{
+    Debug_ASSERT_SELF(self);
+    seos_err_t retval = SEOS_ERROR_GENERIC;
+
+    if (self->isLocalConnection)
+    {
+        retval = SeosKeyStore_moveKey(self->connector.local.keyStore, name, destKeyStore);
+    }
+    else
+    {
+        // implement client call
+    }
+    return retval;
+}
+
+seos_err_t
+SeosKeyStoreApi_generateKey(SeosKeyStoreApi* self, SeosCryptoKey* key, const char* name, char* keyBytes, SeosKeyStore_KeyType* keyType)
+{
+    Debug_ASSERT_SELF(self);
+    seos_err_t retval = SEOS_ERROR_GENERIC;
+
+    if (self->isLocalConnection)
+    {
+        retval = SeosKeyStore_generateKey(self->connector.local.keyStore, key, name, keyBytes, keyType);
+    }
+    else
+    {
+        // implement client call
+    }
+    return retval;
+}
+
