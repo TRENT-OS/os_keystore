5f3c97904e143700db0099235b5ed760de3ec794
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-07-23 11:00:41 +0200

    [REF] SEOS-94: Refactoring the keyStoreApi to improve usability.

--- include/seos/SeosKeyStoreApi.h
+++ include/seos/SeosKeyStoreApi.h
@@ -51,50 +51,39 @@ SeosKeyStoreApi_deInit(SeosKeyStoreApi* self);
 /**
  * @brief Imports a SeosCrypto_Key object into the keystore
  *
- * @param self          pointer to self
- * @param name          name of the key to import
- * @param key           key object to import
+ * @param self              pointer to self
+ * @param key               key object to import
+ * @param keyBytesBuffer    buffer containing the key bytes
+ * @param name              name of the key to import
+ * @param algorithm         algorithm that uses the key
+ * @param flags             flags
+ * @param lenBits           length of the key in bits
  *
  * @return seos_err
  *
  */
 seos_err_t
-SeosKeyStoreApi_importKey(SeosKeyStoreApi*  self,
-                            const char*     name,
-                            SeosCryptoKey*  key);
+SeosKeyStoreApi_importKey(SeosKeyStoreApi*          self,
+                            SeosCrypto_KeyHandle*   key,
+                            const char*             name
+                            void const*             keyBytesBuffer,
+                            unsigned int            algorithm,
+                            unsigned int            flags,
+                            size_t                  lenBits));
 /**
  * @brief Retreives the key with a given name from the keystore
  *
  * @param self          pointer to self
+ * @param key           key handle
  * @param name          name of the key to get
- * @param keyBytes      pointer to the allocated chunk of memory for the keyBytes
- * @param keyBytes      pointer to the key type
- * @param key[out]      the returned key
  *
  * @return seos_err
  *
  */
 seos_err_t
 SeosKeyStoreApi_getKey(SeosKeyStoreApi*         self,
-                        const char*             name,
-                        SeosCryptoKey*          key,
-                        char*                   keyBytes,
-                        SeosKeyStore_KeyType*   keyType);
-/**
- * @brief Reads the key data from the key specified by the passed name and
- * stores the key size in the output parameter keySize
- *
- * @param self          pointer to self
- * @param name          name of the key we want to get
- * @param keySize[out]  the size of the key in bytes
- *
- * @return seos_err
- *
- */
-seos_err_t
-SeosKeyStoreApi_getKeySizeBytes(SeosKeyStoreApi*    self,
-                                const char*         name,
-                                size_t*             keySize);
+                        SeosCrypto_KeyHandle    key,
+                        const char*             name);
 /**
  * @brief Deletes a key with a given name from the keystore
  *
@@ -105,57 +94,59 @@ SeosKeyStoreApi_getKeySizeBytes(SeosKeyStoreApi*    self,
  *
  */
 seos_err_t
-SeosKeyStoreApi_deleteKey(SeosKeyStoreApi*      self,
-                            const char*         name);
+SeosKeyStoreApi_deleteKey(SeosKeyStoreApi*          self,
+                            SeosCrypto_KeyHandle    key);
 /**
  * @brief Copies the key with a selected name from the current key store to
  * the destination key store
  *
  * @param self          pointer to self
- * @param name          name of the key we want to delete
+ * @param key           key handle
  * @param destKeyStore  pointer to the destination key store
  *
  * @return seos_err
  *
  */
 seos_err_t
-SeosKeyStoreApi_copyKey(SeosKeyStoreApi*    self,
-                        const char*         name,
-                        SeosKeyStoreApi*    destKeyStore);
+SeosKeyStoreApi_copyKey(SeosKeyStoreApi*        self,
+                        SeosCrypto_KeyHandle    key,
+                        SeosKeyStoreApi*        destKeyStore);
 /**
  * @brief Moves the key with a selected name from the current key store to
  * the destination key store (after the operation the key is no longer in the
  * current key store)
  *
  * @param self          pointer to self
- * @param name          name of the key we want to delete
+ * @param key           key handle
  * @param destKeyStore  pointer to the destination key store
  *
  * @return seos_err
  *
  */
 seos_err_t
-SeosKeyStoreApi_moveKey(SeosKeyStoreApi*    self,
-                        const char*         name,
-                        SeosKeyStoreApi*    destKeyStore);
+SeosKeyStoreApi_moveKey(SeosKeyStoreApi*        self,
+                        SeosCrypto_KeyHandle    key,
+                        SeosKeyStoreApi*        destKeyStore);
 /**
  * @brief Generates a key with a given name using an RNG, stores the key into the key store
  * and returns the key data in the key object.
  *
  * @param self          pointer to self
- * @param name          name of the key we want to delete
- * @param keyBytes      pointer to the allocated chunk of memory for the keyBytes
- * @param keyBytes      pointer to the key type
- * @param key[out]      the returned key
+ * @param key           key handle
+ * @param name          name of the key to get
+ * @param algorithm     algorithm that uses the key
+ * @param flags         flags
+ * @param lenBits       length of the key in bits
  *
  * @return seos_err
  *
  */
 seos_err_t
 SeosKeyStoreApi_generateKey(SeosKeyStoreApi*        self,
-                            SeosCryptoKey*          key,
+                            SeosCrypto_KeyHandle*   key,
                             const char*             name, 
-                            char*                   keyBytes, 
-                            SeosKeyStore_KeyType*   keyType);
+                            unsigned int            algorithm,
+                            unsigned int            flags,
+                            size_t                  lenBits);
 
 /** @} */
