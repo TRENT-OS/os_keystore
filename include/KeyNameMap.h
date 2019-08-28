267f3e0bb7e78154c83699e611b4e3fec7631396
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-08-28 15:54:29 +0200

    [REF] SEOS-525: Changed the map value type used for the name storage to be of fixed size.

--- include/KeyNameMap.h
+++ include/KeyNameMap.h
@@ -13,29 +13,48 @@
 #include "LibUtil/MapT.h"
 #include "SeosCrypto_Handles.h"
 
-typedef char* SeosKeyStore_KeyName;
+#if !defined(KEYSTORE_CONFIG_H_FILE)
+#   error a configuration file must be provided! See KeyStore_Config.h.example
+#else
+#   define  KeyStore_XSTR(d)    KeyStore_STR(d)
+#   define  KeyStore_STR(d)     #d
+#   include KeyStore_XSTR(KEYSTORE_CONFIG_H_FILE)
+#endif
+
+typedef struct SeosKeyStore_KeyName
+{
+    char buffer[MAX_KEY_NAME_LEN];
+}
+SeosKeyStore_KeyName;
 
 MapT_DECLARE(SeosCrypto_KeyHandle, SeosKeyStore_KeyName, KeyNameMap);
 
 /* Key functions ----------------------------------------------------------*/
 bool
-SeosCrypto_KeyHandle_ctorCopy(SeosCrypto_KeyHandle* dst, SeosCrypto_KeyHandle const* src);
+SeosCrypto_KeyHandle_ctorCopy(SeosCrypto_KeyHandle* dst,
+                              SeosCrypto_KeyHandle const* src);
 bool
-SeosCrypto_KeyHandle_ctorMove(SeosCrypto_KeyHandle* dst, SeosCrypto_KeyHandle const* src);
+SeosCrypto_KeyHandle_ctorMove(SeosCrypto_KeyHandle* dst,
+                              SeosCrypto_KeyHandle const* src);
 bool
-SeosCrypto_KeyHandle_assign(SeosCrypto_KeyHandle* dst, SeosCrypto_KeyHandle const* src);
+SeosCrypto_KeyHandle_assign(SeosCrypto_KeyHandle* dst,
+                            SeosCrypto_KeyHandle const* src);
 void
 SeosCrypto_KeyHandle_dtor(SeosCrypto_KeyHandle* el);
 bool
-SeosCrypto_KeyHandle_isEqual(SeosCrypto_KeyHandle const* a, SeosCrypto_KeyHandle const* b);
+SeosCrypto_KeyHandle_isEqual(SeosCrypto_KeyHandle const* a,
+                             SeosCrypto_KeyHandle const* b);
 
 /* Value functions ----------------------------------------------------------*/
 bool
-SeosKeyStore_KeyName_ctorCopy(SeosKeyStore_KeyName* dst, SeosKeyStore_KeyName const* src);
+SeosKeyStore_KeyName_ctorCopy(SeosKeyStore_KeyName* dst,
+                              SeosKeyStore_KeyName const* src);
 bool
-SeosKeyStore_KeyName_ctorMove(SeosKeyStore_KeyName* dst, SeosKeyStore_KeyName const* src);
+SeosKeyStore_KeyName_ctorMove(SeosKeyStore_KeyName* dst,
+                              SeosKeyStore_KeyName const* src);
 bool
-SeosKeyStore_KeyName_assign(SeosKeyStore_KeyName* dst, SeosKeyStore_KeyName const* src);
+SeosKeyStore_KeyName_assign(SeosKeyStore_KeyName* dst,
+                            SeosKeyStore_KeyName const* src);
 void
 SeosKeyStore_KeyName_dtor(SeosKeyStore_KeyName* el);
 
--- src/KeyNameMap.c
+++ src/KeyNameMap.c
@@ -9,19 +9,22 @@ MapT_DEFINE(SeosCrypto_KeyHandle, SeosKeyStore_KeyName, KeyNameMap);
 
 /* Key functions ----------------------------------------------------------*/
 bool
-SeosCrypto_KeyHandle_ctorCopy(SeosCrypto_KeyHandle* dst, SeosCrypto_KeyHandle const* src)
+SeosCrypto_KeyHandle_ctorCopy(SeosCrypto_KeyHandle* dst,
+                              SeosCrypto_KeyHandle const* src)
 {
     return SeosCrypto_KeyHandle_assign(dst, src);
 }
 
 bool
-SeosCrypto_KeyHandle_ctorMove(SeosCrypto_KeyHandle* dst, SeosCrypto_KeyHandle const* src)
+SeosCrypto_KeyHandle_ctorMove(SeosCrypto_KeyHandle* dst,
+                              SeosCrypto_KeyHandle const* src)
 {
     return SeosCrypto_KeyHandle_assign(dst, src);
 }
 
 bool
-SeosCrypto_KeyHandle_assign(SeosCrypto_KeyHandle* dst, SeosCrypto_KeyHandle const* src)
+SeosCrypto_KeyHandle_assign(SeosCrypto_KeyHandle* dst,
+                            SeosCrypto_KeyHandle const* src)
 {
     *dst = *src;
     return true;
@@ -34,28 +37,32 @@ SeosCrypto_KeyHandle_dtor(SeosCrypto_KeyHandle* el)
 }
 
 bool
-SeosCrypto_KeyHandle_isEqual(SeosCrypto_KeyHandle const* a, SeosCrypto_KeyHandle const* b)
+SeosCrypto_KeyHandle_isEqual(SeosCrypto_KeyHandle const* a,
+                             SeosCrypto_KeyHandle const* b)
 {
     return *a == *b;
 }
 
 /* Value functions ----------------------------------------------------------*/
 bool
-SeosKeyStore_KeyName_ctorCopy(SeosKeyStore_KeyName* dst, SeosKeyStore_KeyName const* src)
+SeosKeyStore_KeyName_ctorCopy(SeosKeyStore_KeyName* dst,
+                              SeosKeyStore_KeyName const* src)
 {
     return SeosKeyStore_KeyName_assign(dst, src);
 }
 
 bool
-SeosKeyStore_KeyName_ctorMove(SeosKeyStore_KeyName* dst, SeosKeyStore_KeyName const* src)
+SeosKeyStore_KeyName_ctorMove(SeosKeyStore_KeyName* dst,
+                              SeosKeyStore_KeyName const* src)
 {
     return SeosKeyStore_KeyName_assign(dst, src);
 }
 
 bool
-SeosKeyStore_KeyName_assign(SeosKeyStore_KeyName* dst, SeosKeyStore_KeyName const* src)
+SeosKeyStore_KeyName_assign(SeosKeyStore_KeyName* dst,
+                            SeosKeyStore_KeyName const* src)
 {
-    *dst = *src;
+    memcpy(dst, src, MAX_KEY_NAME_LEN);
     return true;
 }
 
