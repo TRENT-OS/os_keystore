7316dfcb2f527db3e70d063cdbd371f1d4f29ce7
Carmelo Pintaudi <carmelo.pintaudi@hensoldt-cyber.de>
2021-07-01 17:56:31 +0200

    [NEW] SEOS-2732: Add os_keystore_ram_fv

---
+++ os_keystore_ram_fv/include/OS_KeystoreRamFV.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2021, HENSOLDT Cyber GmbH
+ */
+
+#pragma once
+
+#include "OS_Keystore.int.h"
+#include "KeystoreRamFV.h"
+
+#define OS_KeystoreRamFV_MAX_NAME_LEN \
+    (KeystoreRamFV_KEY_NAME_SIZE - 1)
+
+#define OS_KeystoreRamFV_MAX_KEY_SIZE \
+    (KeystoreRamFV_KEY_DATA_SIZE)
+
+#define OS_KeystoreRamFV_SIZE_OF_BUFFER(num_elements) \
+    ((num_elements) * sizeof(KeystoreRamFV_ElementRecord_t))
+
+#define OS_KeystoreRamFV_NUM_ELEMENTS_BUFFER(size_of_buffer) \
+    ((size_of_buffer) / sizeof(KeystoreRamFV_ElementRecord_t))
+
+#define OS_KeystoreRamFV_TO_OS_KEYSTORE(self)   (&((self)->parent))
+
+
+typedef struct
+{
+    OS_Keystore_t               parent;
+    KeystoreRamFV_t             fvKeystore;
+    unsigned char           buffer[OS_KeystoreRamFV_MAX_KEY_SIZE];
+}
+OS_KeystoreRamFV_t;
+
+
+OS_Error_t
+OS_KeystoreRamFV_init(
+    OS_Keystore_Handle_t*   pHandle,
+    void*                   buf,
+    unsigned                numElements);
---
+++ os_keystore_ram_fv/src/OS_KeystoreRamFV.c
@@ -0,0 +1,447 @@
+/*
+ * Copyright (C) 2021, HENSOLDT Cyber GmbH
+ */
+
+#include "OS_KeystoreRamFV.h"
+
+#include "lib_debug/Debug.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// The formally verified keystore offers the chance to map a key by the pair
+// name-appId. We are interested only in name, therefore we will always use the
+// same appId here defined.
+#define APP_ID 0
+
+
+// Vtable definition -----------------------------------------------------------
+
+static OS_Error_t
+OS_KeystoreRamFV_free(
+    OS_Keystore_t* ptr);
+
+static OS_Error_t
+OS_KeystoreRamFV_storeKey(
+    OS_Keystore_t*  ptr,
+    const char*     name,
+    void const*     keyData,
+    size_t          keySize);
+
+static OS_Error_t
+OS_KeystoreRamFV_loadKey(
+    OS_Keystore_t*  ptr,
+    const char*     name,
+    void*           keyData,
+    size_t*         keySize);
+
+static OS_Error_t
+OS_KeystoreRamFV_deleteKey(
+    OS_Keystore_t*  ptr,
+    const char*     name);
+
+static OS_Error_t
+OS_KeystoreRamFV_copyKey(
+    OS_Keystore_t*  srcPtr,
+    const char*     name,
+    OS_Keystore_t*  dstPtr);
+
+static OS_Error_t
+OS_KeystoreRamFV_moveKey(
+    OS_Keystore_t*  srcPtr,
+    const char*     name,
+    OS_Keystore_t*  dstPtr);
+
+static OS_Error_t
+OS_KeystoreRamFV_wipeKeystore(
+    OS_Keystore_t*  ptr);
+
+static const OS_Keystore_Vtable_t OS_KeystoreRamFV_vtable =
+{
+    .free           = OS_KeystoreRamFV_free,
+    .storeKey       = OS_KeystoreRamFV_storeKey,
+    .loadKey        = OS_KeystoreRamFV_loadKey,
+    .deleteKey      = OS_KeystoreRamFV_deleteKey,
+    .copyKey        = OS_KeystoreRamFV_copyKey,
+    .moveKey        = OS_KeystoreRamFV_moveKey,
+    .wipeKeystore   = OS_KeystoreRamFV_wipeKeystore
+};
+
+
+// Private functions -----------------------------------------------------------
+
+static inline bool
+isLoadKeyParametersOk(
+    OS_KeystoreRamFV_t* self,
+    const char*         name,
+    void*               keyData,
+    size_t*             keySize)
+{
+    if (NULL == self || NULL == keySize || NULL == keyData || NULL == name)
+    {
+        return false;
+    }
+
+    size_t nameLen = strlen(name);
+
+    if (nameLen > OS_KeystoreRamFV_MAX_NAME_LEN || nameLen == 0)
+    {
+        Debug_LOG_ERROR("%s: The length of the passed key name %zu is invalid, must be in the range [1;%d]!",
+                        __func__, nameLen, OS_KeystoreRamFV_MAX_NAME_LEN);
+        return false;
+    }
+
+    size_t my_keysize = *keySize;
+
+    if (my_keysize > OS_KeystoreRamFV_MAX_KEY_SIZE)
+    {
+        Debug_LOG_ERROR("%s: The length of the passed key data %zu is invalid, must be in the range [1;%d]!",
+                        __func__, my_keysize, OS_KeystoreRamFV_MAX_KEY_SIZE);
+        return false;
+    }
+
+    return true;
+}
+
+static inline bool
+isStoreKeyParametersOk(
+    OS_KeystoreRamFV_t* self,
+    const char*         name,
+    void const*         keyData,
+    size_t              keySize)
+{
+    if (NULL == self || NULL == keyData || NULL == name)
+    {
+        return false;
+    }
+
+    size_t nameLen = strlen(name);
+
+    if (nameLen > OS_KeystoreRamFV_MAX_NAME_LEN || nameLen == 0)
+    {
+        Debug_LOG_ERROR("%s: The length of the passed key name %zu is invalid, must be in the range [1;%d]!",
+                        __func__,
+                        nameLen,
+                        OS_KeystoreRamFV_MAX_NAME_LEN);
+        return false;
+    }
+
+    if (keySize > OS_KeystoreRamFV_MAX_KEY_SIZE || keySize == 0)
+    {
+        Debug_LOG_ERROR("%s: The length of the passed key data %zu is invalid, must be in the range [1;%d]!",
+                        __func__, keySize, OS_KeystoreRamFV_MAX_KEY_SIZE);
+        return false;
+    }
+
+    return true;
+}
+
+static OS_Error_t
+ctor(
+    OS_KeystoreRamFV_t* self,
+    void*               buf,
+    unsigned            numElements)
+{
+    if (NULL == self)
+    {
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    memset(self, 0, sizeof(OS_KeystoreRamFV_t));
+    KeystoreRamFV_init(&self->fvKeystore, numElements, buf);
+
+    OS_KeystoreRamFV_TO_OS_KEYSTORE(self)->vtable = &OS_KeystoreRamFV_vtable;
+
+    return OS_SUCCESS;
+}
+
+static OS_Error_t
+dtor(
+    OS_KeystoreRamFV_t* self)
+{
+    if (self == NULL)
+    {
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    return OS_SUCCESS;
+}
+
+
+// Exported via Vtable ---------------------------------------------------------
+
+static OS_Error_t
+OS_KeystoreRamFV_free(
+    OS_Keystore_t*  ptr)
+{
+    OS_KeystoreRamFV_t* self = (OS_KeystoreRamFV_t*) ptr;
+
+    OS_Error_t err = dtor(self);
+    if (OS_SUCCESS == err)
+    {
+        free(self);
+    }
+
+    return err;
+}
+
+static OS_Error_t
+OS_KeystoreRamFV_storeKey(
+    OS_Keystore_t*  ptr,
+    const char*     name,
+    void const*     keyData,
+    size_t          keySize)
+{
+    OS_KeystoreRamFV_t* self = (OS_KeystoreRamFV_t*) ptr;
+
+    if (!isStoreKeyParametersOk(self, name, keyData, keySize))
+    {
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    key_record_t keyRecord = { 0 };
+
+    strncpy(keyRecord.name, name, sizeof(keyRecord.name) - 1);
+    memcpy(keyRecord.data, keyData, keySize);
+
+    KeystoreRamFV_Result_t result = KeystoreRamFV_add(
+                                        &self->fvKeystore,
+                                        APP_ID,
+                                        &keyRecord);
+    if (result.error)
+    {
+        Debug_LOG_ERROR("%s: key_store_add() failed, err %d!",
+                        __func__,
+                        result.error);
+        return result.error == KeystoreRamFV_ERR_OUT_OF_SPACE ?
+               OS_ERROR_INSUFFICIENT_SPACE : OS_ERROR_INVALID_PARAMETER;
+    }
+
+    return OS_SUCCESS;
+}
+
+static OS_Error_t
+OS_KeystoreRamFV_loadKey(
+    OS_Keystore_t*  ptr,
+    const char*     name,
+    void*           keyData,
+    size_t*         keySize)
+{
+    OS_KeystoreRamFV_t* self = (OS_KeystoreRamFV_t*) ptr;
+
+    if (!isLoadKeyParametersOk(self, name, keyData, keySize))
+    {
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    key_record_t keyRecord = { 0 };
+
+    // NOTE: The formally verified keystore does not support null-terminated
+    // strings but always compares the whole char-array. For adaption a char
+    // array of max size needs to be filled with 0 after the name.
+    char cleanName[KeystoreRamFV_KEY_NAME_SIZE] = { 0 };
+    strncpy(cleanName, name, sizeof(cleanName) - 1);
+
+    KeystoreRamFV_Result_t result = KeystoreRamFV_get(
+                                        &self->fvKeystore,
+                                        APP_ID,
+                                        cleanName,
+                                        &keyRecord);
+    if (result.error)
+    {
+        *keySize = 0;
+        Debug_LOG_ERROR("%s: key_store_add() failed, err %d!",
+                        __func__,
+                        result.error);
+        return result.error == KeystoreRamFV_ERR_NOT_FOUND ?
+               OS_ERROR_NOT_FOUND : OS_ERROR_INVALID_PARAMETER;
+    }
+
+    if (*keySize == 0)
+    {
+        return OS_ERROR_BUFFER_TOO_SMALL;
+    }
+
+    *keySize =
+        *keySize < sizeof(keyRecord.data) ? *keySize : sizeof(keyRecord.data);
+    memcpy(keyData, keyRecord.data, *keySize);
+
+    return OS_SUCCESS;
+}
+
+static OS_Error_t
+OS_KeystoreRamFV_deleteKey(
+    OS_Keystore_t*  ptr,
+    const char*     name)
+{
+    OS_KeystoreRamFV_t* self = (OS_KeystoreRamFV_t*) ptr;
+
+    if (NULL == self || NULL == name)
+    {
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    size_t nameLen = strlen(name);
+
+    if (nameLen > OS_KeystoreRamFV_MAX_NAME_LEN || nameLen == 0)
+    {
+        Debug_LOG_ERROR("%s: The length of the passed key name %zu is invalid, must be in the range [1;%d]!",
+                        __func__,
+                        nameLen,
+                        OS_KeystoreRamFV_MAX_NAME_LEN);
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    // NOTE: The formally verified keystore does not support null-terminated
+    // strings but always compares the whole char-array. For adaption a char
+    // array of max size needs to be filled with 0 after the name.
+    char cleanName[KeystoreRamFV_KEY_NAME_SIZE] = { 0 };
+    strncpy(cleanName, name, sizeof(cleanName) - 1);
+
+    unsigned int err = KeystoreRamFV_delete(
+                           &self->fvKeystore,
+                           APP_ID,
+                           cleanName);
+    if (err)
+    {
+        return err == KeystoreRamFV_ERR_NOT_FOUND ?
+               OS_ERROR_NOT_FOUND : OS_ERROR_INVALID_PARAMETER;
+    }
+
+    return OS_SUCCESS;
+}
+
+static OS_Error_t
+OS_KeystoreRamFV_copyKey(
+    OS_Keystore_t*  srcPtr,
+    const char*     name,
+    OS_Keystore_t*  dstPtr)
+{
+    OS_KeystoreRamFV_t* self = (OS_KeystoreRamFV_t*) srcPtr;
+    size_t keySize = OS_KeystoreRamFV_MAX_KEY_SIZE;
+
+    if (NULL == self || NULL == name || NULL == dstPtr)
+    {
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    size_t nameLen = strlen(name);
+
+    if (nameLen > OS_KeystoreRamFV_MAX_NAME_LEN || nameLen == 0)
+    {
+        Debug_LOG_ERROR("%s: The length of the passed key name %zu is invalid, must be in the range [1;%d]!",
+                        __func__,
+                        nameLen,
+                        OS_KeystoreRamFV_MAX_NAME_LEN);
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    OS_Error_t err = OS_Keystore_loadKey(srcPtr, name, self->buffer, &keySize);
+
+    if (err != OS_SUCCESS)
+    {
+        Debug_LOG_ERROR("%s: loadKey failed with err %d!", __func__, err);
+        return err;
+    }
+
+    err = OS_Keystore_storeKey(dstPtr, name, self->buffer, keySize);
+
+    if (err != OS_SUCCESS)
+    {
+        Debug_LOG_ERROR("%s: storeKey failed with err %d!", __func__, err);
+        return err;
+    }
+
+    return err;
+}
+
+static OS_Error_t
+OS_KeystoreRamFV_moveKey(
+    OS_Keystore_t*  srcPtr,
+    const char*     name,
+    OS_Keystore_t*  dstPtr)
+{
+    OS_Error_t err;
+
+    if (NULL == srcPtr || NULL  == name || NULL == dstPtr)
+    {
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    size_t nameLen = strlen(name);
+
+    if (nameLen > OS_KeystoreRamFV_MAX_NAME_LEN || nameLen == 0)
+    {
+        Debug_LOG_ERROR("%s: The length of the passed key name %zu is invalid, must be in the range [1;%d]!",
+                        __func__,
+                        nameLen,
+                        OS_KeystoreRamFV_MAX_NAME_LEN);
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    err = OS_Keystore_copyKey(srcPtr, name, dstPtr);
+
+    if (err != OS_SUCCESS)
+    {
+        Debug_LOG_ERROR("%s: copyKey failed with err %d!", __func__, err);
+        return err;
+    }
+
+    err = OS_Keystore_deleteKey(srcPtr, name);
+
+    if (err != OS_SUCCESS)
+    {
+        Debug_LOG_ERROR("%s: deleteKey failed with err %d!", __func__, err);
+        return err;
+    }
+
+    return err;
+}
+
+static OS_Error_t
+OS_KeystoreRamFV_wipeKeystore(
+    OS_Keystore_t*  ptr)
+{
+    OS_KeystoreRamFV_t* self = (OS_KeystoreRamFV_t*) ptr;
+
+    if (NULL == self)
+    {
+        return OS_ERROR_INVALID_PARAMETER;
+    }
+
+    KeystoreRamFV_wipe(&self->fvKeystore);
+
+    return OS_SUCCESS;
+}
+
+
+// Public functions ------------------------------------------------------------
+
+OS_Error_t
+OS_KeystoreRamFV_init(
+    OS_Keystore_Handle_t*   pHandle,
+    void*                   buf,
+    unsigned                numElements)
+{
+    OS_Error_t err = OS_ERROR_GENERIC;
+    OS_KeystoreRamFV_t* self = malloc(sizeof(OS_KeystoreRamFV_t));
+
+    if (NULL == self)
+    {
+        return OS_ERROR_INSUFFICIENT_SPACE;
+    }
+
+    err = ctor(self, buf, numElements);
+
+    if (err != OS_SUCCESS)
+    {
+        free(self);
+    }
+    else
+    {
+        *pHandle = OS_KeystoreRamFV_TO_OS_KEYSTORE(self);
+    }
+
+    return err;
+}
